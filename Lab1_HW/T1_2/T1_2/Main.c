2868b30dd27edb421a3c75eab59a644350cea275
D4vv1dd <standavid014@gmail.com>
2025-10-23 01:34:10 +0300

    Lab 1

--- Lab1_HW/T1_2/T1_2/Main.c
+++ Lab1_HW/T1_2/T1_2/Main.c
@@ -3,7 +3,7 @@
 #define USE_Header1_h 1 /*Change to 0 to use Header2*/
 
 /*
-	#if,#else,#endif lets us choose the header by setting the macro, making it easier to switch between them...
+	#if,#else,#endif lets us choose the header by setting the macro, making it easier to switch between them.
 */
 
 #if USE_Header1_h
